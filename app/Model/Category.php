8844c05d96827f870db368ac5299556f7dcaa581
Anthony dekoci <dekoci@yahoo.com>
2018-11-21 23:19:33 +0700

    Set relation with products on Category model

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -25,5 +25,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Category extends Model
 {
-    //
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
