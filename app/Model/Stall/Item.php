afd97bfe80f78e3ca2def55415b3ec82c2441edc
Anthony dekoci <dekoci@yahoo.com>
2018-11-22 10:48:34 +0700

    Add model Stall/Item which is them store_items table

---
+++ app/Model/Stall/Item.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model\Stall;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Model\Stall\Item
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Stall\Item newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Stall\Item newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Model\Stall\Item query()
+ * @mixin \Eloquent
+ */
+class Item extends Model
+{
+    protected $table = 'store_items';
+}
