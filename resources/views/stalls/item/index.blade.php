9450e684b0dca4565c93d43c97ad946e4f806aa2
Fathur Rohman <hi.fathur.rohman@gmail.com>
2018-12-06 18:26:26 +0700

    store item

--- resources/views/stalls/item/index.blade.php
+++ resources/views/stalls/item/index.blade.php
@@ -12,12 +12,12 @@
     <div class="panel shadow">
         <div class="panel-heading">
             <div class="pull-left">
-                <h3 class="panel-title">Stall Items</h3>
+                <h3 class="panel-title">Store Items</h3>
             </div>
             <div class="pull-right">
-                <button type="button" class="btn btn-primary" data-toggle="modal" id="btn-stallitem-add">
+               {{-- <button type="button" class="btn btn-primary" data-toggle="modal" id="btn-stallitem-add">
                     <i class="fa fa-plus"></i> Add Item
-                </button>
+                </button>--}}
             </div>
             <div class="clearfix"></div>
         </div>
@@ -26,7 +26,15 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="table-responsive">
-                        {!! $dataTable->table(['class' => 'table table-bordered table-striped table-hover','id' => 'dataTables-stallitem-list']) !!}
+{{--                        {!! $dataTable->table(['class' => 'table table-bordered table-striped table-hover','id' => 'dataTables-stallitem-list']) !!}--}}
+                        {{--<table class="table table-bordered table-striped">--}}
+                            {{--<thead>--}}
+                            {{--<tr>--}}
+                                {{--<th>#</th>--}}
+                                {{----}}
+                            {{--</tr>--}}
+                            {{--</thead>--}}
+                        {{--</table>--}}
                     </div>
                 </div>
             </div>
@@ -51,7 +59,7 @@
     <script>
 
         $('#btn-stallitem-add').click(function () {
-            create("{{route('stallitem.create')}}");
+            create("{{route('store-item.create')}}");
         });
 
     </script>
