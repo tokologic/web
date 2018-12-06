6f3109928291976f6b722763bb6f5d5270c18e38
Fathur Rohman <hi.fathur.rohman@gmail.com>
2018-12-06 18:29:01 +0700

    store item edit

--- resources/views/stalls/item/action.blade.php
+++ resources/views/stalls/item/action.blade.php
@@ -1,10 +1,10 @@
 <div class="btn-group btn-group-xs" role="group" aria-label="Basic example">
-    <button type="button" class="btn btn-warning " data-route="{{route('stallitem.edit', $item->id)}}" onclick="edit(this)">
+    <button type="button" class="btn btn-warning " data-route="{{route('store-item.edit', $item->id)}}" onclick="edit(this)">
         <i class="fa fa-pencil"></i> Edit
     </button>
-    <button type="button" class="btn btn-danger" data-route="{{route('stallitem.destroy', $item->id)}}"
+    {{--<button type="button" class="btn btn-danger" data-route="{{route('store-item.destroy', $item->id)}}"
             data-table="dataTables-stallitem-list"
             data-token="{{csrf_token()}}"
             onclick="destroy(this)"> <i class="fa fa-trash"></i> Destroy
-    </button>
+    </button>--}}
 </div>
